656da5301daf84311ae11e8783b27834ffdd928e
Rabia Arif <rabia.arif@infotechgroup.com>
2023-02-20 14:39:01 +0500

    scope checking of shared and unique pointer

--- SmartPointers/SmartPointers/UniquePointer.cpp
+++ SmartPointers/SmartPointers/UniquePointer.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-using namespace std;
+//using namespace std;
 #include <memory>
 
 class Rectangle {
@@ -10,11 +10,11 @@ public:
 	Rectangle(int l, int b) {
 		length = l;
 		breadth = b;
-		cout << "\nConstructor of Rectangle is called"<< endl;
+		std::cout << "\nConstructor of Rectangle is called"<< std::endl;
 	}
 
 	~Rectangle() {
-		cout << "\nDestructor of Rectangle is called\n";
+		std::cout << "\nDestructor of Rectangle is called\n";
 	}
 	int area() {
 		return length * breadth;
@@ -22,20 +22,20 @@ public:
 	//copy constructor
 	Rectangle(const Rectangle& pt)
 	{
-		cout << "\nin Rectangle copy  constructor";
+		std::cout << "\nin Rectangle copy  constructor";
 	}
 	//move constructor
 	Rectangle(Rectangle&& pt)
 	{
-		cout << "\nin Rectangle Move constructor";
+		std::cout << "\nin Rectangle Move constructor";
 	}
 	
 	Rectangle& operator*() {
-		cout << "\nin \"*\" operator ";
+		std::cout << "\nin \"*\" operator ";
 
 	}
 	Rectangle& operator=(Rectangle pt) {
-		cout << "\nin \"=\" operator ";
+		std::cout << "\nin \"=\" operator ";
 
 		return pt;
 	}
@@ -46,99 +46,140 @@ public:
 	
 };
 
+
+//void printPointer(std::unique_ptr<Rectangle> rect1)
+//{
+//	std::cout << "\nP1 area: " << rect1->area() << std::endl; // This'll print 50
+//	std::cout << "\nP1.get(): " << rect1.get() << std::endl;
+//}
+
 int main() {
 
-	unique_ptr<Rectangle> P1(new Rectangle(10, 5));
-	cout << "\nP1 area: " << P1->area() << endl; // This'll print 50
-	cout << "\nP1.get(): " << P1.get() << endl;
+	std::unique_ptr<Rectangle> P1(new Rectangle(10, 5));
+	std::cout << "\nP1 area: " << P1->area() << std::endl; // This'll print 50
+	std::cout << "\nP1.get(): " << P1.get() << std::endl;
+	
+	{
+
+		std::cout << "\nP1 area: inside loop " << P1->area() << std::endl; // This'll print 50
+		std::cout << "\nP1.get(): " << P1.get() << std::endl;
+
+		std::unique_ptr<Rectangle> P2;
+		std::cout << "\nP2.get(): " << P2.get() << std::endl;
+		P2 = std::move(P1);
+		std::cout << "\nP2 area: " << P2->area() << std::endl;
+		std::cout << "\nP1.get(): " << P1.get() << std::endl;
+
+	//cout << "\nP2 area: " << P2->area() << endl;
+	}
+	std::cout << "\nP2 area: " << P2->area() << std::endl;
+	std::cout << "\nP1.get(): " << P1.get() << std::endl;
+
 	
 	//unique_ptr<Rectangle> P2(P1); // error: cannot be reffered -- it is a deleted function
 	
-	unique_ptr<Rectangle> P2;
-	cout << "\nP2.get(): " << P2.get() << endl;
-	//cout << "\nP2 area: " << P2->area() << endl;
+	//std::unique_ptr<Rectangle> P2;
+	//std::cout << "\nP2.get(): " << P2.get() << std::endl;
+	////cout << "\nP2 area: " << P2->area() << endl;
 
-	//P2 = P1;  // error: cannot be reffered -- it is a deleted function
+	////P2 = P1;  // error: cannot be reffered -- it is a deleted function
 
-	P2 = std::move(P1);
+	//P2 = std::move(P1);
 
-	// This'll print 50
-	cout << "\nP2 area: " << P2->area() << endl;
-	cout << "\nP1.get(): " << P1.get() << endl;
-	cout << "\nP2.get(): " << P2.get() << endl;
-	cout << "\nP1.get(): " << P1.get() << endl;
-	//cout <<  "\nP1 area: " << P1->area() << endl; //error ....solution: shared pointer
+	//// This'll print 50
+	//std::cout << "\nP2 area: " << P2->area() << std::endl;
+	//std::cout << "\nP1.get(): " << P1.get() << std::endl;
+	//std::cout << "\nP2.get(): " << P2.get() << std::endl;
+	//std::cout << "\nP1.get(): " << P1.get() << std::endl;
+	////cout <<  "\nP1 area: " << P1->area() << endl; //error ....solution: shared pointer
 
 
 
-	Rectangle rect123(11, 11);
-	//cout << rect123->area() << endl;
+	//Rectangle rect123(11, 11);
+	////cout << rect123->area() << endl;
 
-	Rectangle rect13(117, 11);
+	//Rectangle rect13(117, 11);
 	//cout << P2->area() << endl;
 	/*for (int i = 0; i < 1 ; i++)
 	{
 		cout << "\nP1.get() inside loop: " << P1.get() << endl;
 	}*/
-	unique_ptr<Rectangle> unique_Rect1 = std::make_unique<Rectangle>(rect123); //goes to copy constructor
-	cout << "\nunique_Rect1.get(): " << unique_Rect1.get() << endl;
+	//std::unique_ptr<Rectangle> unique_Rect1 = std::make_unique<Rectangle>(rect123); //goes to copy constructor
+	//std::cout << "\nunique_Rect1.get(): " << unique_Rect1.get() << std::endl;
 
-	unique_ptr<Rectangle> unique_Rect2; //goes to copy constructor
+	//std::unique_ptr<Rectangle> unique_Rect2; //goes to copy constructor
 
-	//unique_ptr<Rectangle> unique_Rect2 = std::make_unique<Rectangle>(rect13); //goes to copy constructor
-	unique_Rect2 = std::move(unique_Rect1);
+	////unique_ptr<Rectangle> unique_Rect2 = std::make_unique<Rectangle>(rect13); //goes to copy constructor
+	//unique_Rect2 = std::move(unique_Rect1);
 
-	cout << "\nunique_Rect2.get(): " << unique_Rect2.get() << endl;
-	cout << "\nunique_Rect1.get(): " << unique_Rect1.get() << endl;
+	//std::cout << "\nunique_Rect2.get(): " << unique_Rect2.get() << std::endl;
+	//std::cout << "\nunique_Rect1.get(): " << unique_Rect1.get() << std::endl;
 
 	 //cout<<P1->area()<<endl;
 
 
 
-	//................Shared Pointer....................//
+	////................Shared Pointer....................//
 
-	cout << "\n\n\n\n.....Shared Pointer.........\n\n";
-	Rectangle rectShared1(11, 11);
-	Rectangle rectShared2(10, 10);
-	
-	std::shared_ptr<Rectangle> sharedRectPointer = std::make_shared<Rectangle>(rectShared1);
+	//std::cout << "\n\n\n\n.....Shared Pointer.........\n\n";
 
-	cout << "\n\nsharedRectPointer.get(): " << sharedRectPointer.get() << endl;
-	cout << "\nsharedRectPointer.use_count(): " << sharedRectPointer.use_count() << endl ;
-	
-	std::shared_ptr<Rectangle> sharedRectPointer2;
+	//std::shared_ptr<Rectangle> sharedRectPointer = std::make_shared<Rectangle>(Rectangle(11, 11));
 
-	cout << "\n\nsharedRectPointer2.get(): " << sharedRectPointer2.get() << endl;
-	cout << "\nsharedRectPointer2.use_count(): " << sharedRectPointer2.use_count() << endl;
+	//std::cout << "\n\nsharedRectPointer.get(): " << sharedRectPointer.get() << std::endl;
+	//std::cout << "\nsharedRectPointer.use_count(): " << sharedRectPointer.use_count() << std::endl;
 
-	sharedRectPointer2 = sharedRectPointer;
 
-	cout << "\n\nsharedRectPointer.get(): " << sharedRectPointer.get() << endl;
-	cout << "\nsharedRectPointer.use_count(): " << sharedRectPointer.use_count() << endl;
+	//{
+	//	std::cout << "\n\nInside Scope:,..............." << std::endl;
 
-	cout << "\n\nsharedRectPointer2.get(): " << sharedRectPointer2.get() << endl;
-	cout << "\nsharedRectPointer2.use_count(): " << sharedRectPointer2.use_count() << endl;
+	//	std::shared_ptr<Rectangle> sharedRectPointer2;
 
-	std::shared_ptr<Rectangle> sharedRectPointer3;
-	cout << "\n\nsharedRectPointer3.get(): " << sharedRectPointer3.get() << endl;
+	//	std::cout << "\n\nsharedRectPointer2.get(): " << sharedRectPointer2.get() << std::endl;
+	//	std::cout << "\nsharedRectPointer2.use_count(): " << sharedRectPointer2.use_count() << std::endl;
 
-	cout << "\n\nsharedRectPointer3.get(): " << sharedRectPointer3.get() << endl;
-	cout << "\nsharedRectPointer3.use_count(): " << sharedRectPointer3.use_count() << endl;
+	//	sharedRectPointer2 = sharedRectPointer; //sharedRectPointer can be accessed here
 
-	sharedRectPointer3 = sharedRectPointer;
+	//	std::cout << "\n\nsharedRectPointer.get(): " << sharedRectPointer.get() << std::endl;
+	//	std::cout << "\nsharedRectPointer.use_count(): " << sharedRectPointer.use_count() << std::endl;
 
-	cout << "\n\nsharedRectPointer.get(): " << sharedRectPointer.get() << endl;
-	cout << "\nsharedRectPointer.use_count(): " << sharedRectPointer.use_count() << endl;
+	//	std::cout << "\n\nsharedRectPointer2.get(): " << sharedRectPointer2.get() << std::endl;
+	//	std::cout << "\nsharedRectPointer2.use_count(): " << sharedRectPointer2.use_count() << std::endl;
 
-	cout << "\n\nsharedRectPointer3.get(): " << sharedRectPointer3.get() << endl;
-	cout << "\nsharedRectPointer3.use_count(): " << sharedRectPointer3.use_count() << endl;
+	//}
 
-	cout << "\n\n\n\n\n\n";
+	//sharedRectPointer2 cannot be accesed here
+	// 
+	//std::cout << "\n\nsharedRectPointer2.get(): " << sharedRectPointer2.get() << std::endl;
+	//std::cout << "\nsharedRectPointer2.use_count(): " << sharedRectPointer2.use_count() << std::endl;
+
+
+	//std::shared_ptr<Rectangle> sharedRectPointer2;
+
+	//std::cout << "\n\nsharedRectPointer2.get(): " << sharedRectPointer2.get() << std::endl;
+	//std::cout << "\nsharedRectPointer2.use_count(): " << sharedRectPointer2.use_count() << std::endl;
+
+	//sharedRectPointer2 = sharedRectPointer;
+
+	//std::cout << "\n\nsharedRectPointer.get(): " << sharedRectPointer.get() << std::endl;
+	//std::cout << "\nsharedRectPointer.use_count(): " << sharedRectPointer.use_count() << std::endl;
+
+	//std::cout << "\n\nsharedRectPointer2.get(): " << sharedRectPointer2.get() << std::endl;
+	//std::cout << "\nsharedRectPointer2.use_count(): " << sharedRectPointer2.use_count() << std::endl;
+
+	//std::shared_ptr<Rectangle> sharedRectPointer3;
+	//std::cout << "\n\nsharedRectPointer3.get(): " << sharedRectPointer3.get() << std::endl;
+
+	//std::cout << "\n\nsharedRectPointer3.get(): " << sharedRectPointer3.get() << std::endl;
+	//std::cout << "\nsharedRectPointer3.use_count(): " << sharedRectPointer3.use_count() << std::endl;
+
+	//sharedRectPointer3 = sharedRectPointer;
+
+	//std::cout << "\n\nsharedRectPointer.get(): " << sharedRectPointer.get() << std::endl;
+	//std::cout << "\nsharedRectPointer.use_count(): " << sharedRectPointer.use_count() << std::endl;
+
+	//std::cout << "\n\nsharedRectPointer3.get(): " << sharedRectPointer3.get() << std::endl;
+	//std::cout << "\nsharedRectPointer3.use_count(): " << sharedRectPointer3.use_count() << std::endl;
+	std::cout << "\n\n\n\n\n\n";
 
 	return 0;
 }
-
-//void func() {
-//	cout << "In Func";
-//	P1->
-//}
